a71f604223b41c4df820470d9d9655ff8b1d2ab9
Antonio Insuasti <efainsua@pichincha.com>
2022-10-14 21:24:03 -0500

    cleaning

--- Hybrid.dart
+++ Hybrid.dart
@@ -1,13 +1,10 @@
 import 'Combustion.dart';
 
-
 class Hybrid extends Combustion {
   int? bateryCapacity;
 
-    // sigue sin gustarme
-  
-  Hybrid(super.brand, super.model, super.year, super.fuelType, this.bateryCapacity );
+  // sigue sin gustarme
 
- 
-
+  Hybrid(super.brand, super.model, super.year, super.fuelType,
+      this.bateryCapacity);
 }
